06a21b008d983c7cc713ac221395fa44fb0f0247
LLSJ <122068486+PETROMYZONMONSTER@users.noreply.github.com>
2024-08-18 23:23:12 -0300

    UPDATED SUBMIT FORM

--- PROGRAMAÇÃO PARA WEB/script.js
+++ PROGRAMAÇÃO PARA WEB/script.js
@@ -1,2 +1,21 @@
+let SubmitButton = document.getElementById("onclick");
 
+SubmitButton.addEventListener("on-click", submit);
+
+function submit() {
+    let NameElement = document.getElementById("Name").value;
+    let AgeElement = document.getElementById("Age").value;
+    let EmailElement = document.getElementById("E-mail").value;
+
+    let EmailPattern = /^[\w\.-]+@\w+(\.\w+)+$/;
+
+    if (EmailPattern.test(EmailElement)) {
+        console.log("Valid E-mail format:", EmailElement);
+    } else {
+        console.log("Invalid E-mail format:", EmailElement);
+    }
+
+    console.log(“Name value:”NameElement);
+    console.log(“Age value:”AgeElement);
+}
 
